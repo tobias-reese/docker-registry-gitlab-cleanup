64c7744dc3ca7863f0f419e50a87bd34808261c9
Jiří Pinkava <j-pi@seznam.cz>
2018-09-20 13:23:16 +0200

    flatten
    remove nested ifs, use continue, simplify

--- rgc/gitlab/clean.py
+++ rgc/gitlab/clean.py
@@ -28,41 +28,42 @@ class GitlabClean( object ):
 
         print( '-> loading all projects..' )
         for project in gitlab.Gitlab( self.gitlab_url, self.token ).projects.list( all=True ):
-            if project.container_registry_enabled:
-                subimages = []
-                for image in images:
-                    if project.path_with_namespace.lower() == image or \
-                            image.startswith(project.path_with_namespace.lower() + '/'):
-                        subimages.append(image)
-
-                for subimage in subimages:
-                    print( '-> processing ' + subimage )
-                    query_tags = registry.query( self.registry_url + '/v2/' + subimage + '/tags/list', 'get' )
-                    tags = query_tags.get('tags', [])
-
-                    if tags:
-                        print( '--> ' + str( len( tags ) ) + ' tag(s) found' )
-                        for tag in tags:
-                            if not re.match( self.exclude, tag ):
-                                # BUG: Sometimes the 'history' field is not available, usally works on next try
-                                tag_info = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'get' )
-                                try:
-                                    tag_info['history']
-                                except KeyError:
-                                    print( colored( '--> couldn\'t get date info for ' + tag + ' (skipped)', 'yellow' ) )
-                                else:
-                                    created_at = datetime.strptime( json.loads( tag_info['history'][0]['v1Compatibility'] )['created'][:-4], '%Y-%m-%dT%H:%M:%S.%f' )
-                                    age = now - created_at
-                                    if age.total_seconds() > ( int( self.retention ) * 60 * 60 * 24 ):
-                                        print( colored( '--> removing ' + tag + ' (expired)', 'red' ) )
-                                        digest = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'head' )['Docker-Content-Digest']
-                                        registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + digest, 'delete' )
-                                    else:
-                                        print( colored( '--> keeping ' + tag + ' (not expired)', 'green' ) )
-                            else:
-                                print( colored( '--> keeping ' + tag + ' (excluded)', 'green' ) )
-                    else:
-                        print( '--> no tags' )
-
-            else:
+            if not project.container_registry_enabled:
                 print( '-> skipping ' + project.path_with_namespace.lower() )
+                continue
+
+            subimages = []
+            for image in images:
+                if project.path_with_namespace.lower() == image or \
+                        image.startswith(project.path_with_namespace.lower() + '/'):
+                    subimages.append(image)
+
+            for subimage in subimages:
+                print( '-> processing ' + subimage )
+                query_tags = registry.query( self.registry_url + '/v2/' + subimage + '/tags/list', 'get' )
+                tags = query_tags.get('tags', [])
+
+                if not tags:
+                    print( '--> no tags' )
+                    continue
+
+                print( '--> ' + str( len( tags ) ) + ' tag(s) found' )
+                for tag in tags:
+                    if re.match( self.exclude, tag ):
+                        print( colored( '--> keeping ' + tag + ' (excluded)', 'green' ) )
+                        continue
+
+                    # BUG: Sometimes the 'history' field is not available, usally works on next try
+                    tag_info = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'get' )
+                    if not 'history' in tag_info:
+                        print( colored( '--> couldn\'t get date info for ' + tag + ' (skipped)', 'yellow' ) )
+                        continue
+
+                    created_at = datetime.strptime( json.loads( tag_info['history'][0]['v1Compatibility'] )['created'][:-4], '%Y-%m-%dT%H:%M:%S.%f' )
+                    age = now - created_at
+                    if age.total_seconds() > ( int( self.retention ) * 60 * 60 * 24 ):
+                        print( colored( '--> removing ' + tag + ' (expired)', 'red' ) )
+                        digest = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'head' )['Docker-Content-Digest']
+                        registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + digest, 'delete' )
+                    else:
+                        print( colored( '--> keeping ' + tag + ' (not expired)', 'green' ) )
