3849ca568ee9d2adb81421b1924f6d475a09d01a
Tobias Reese <tobias.reese@phimobile.com>
2018-11-26 11:35:34 +0100

    added project filter

--- rgc/cli.py
+++ rgc/cli.py
@@ -3,23 +3,35 @@ import os
 import sys
 from rgc.gitlab.clean import GitlabClean
 
+
 def main():
-    parser = argparse.ArgumentParser( description='Cleanup old tags from GitLab Docker Registry' )
-    parser.add_argument( '--user',         '-u', metavar='RGC_USER',         default=os.environ.get('RGC_USER', None),         help='Username of the account used to make the request' )
-    parser.add_argument( '--token',        '-t', metavar='RGC_TOKEN',        default=os.environ.get('RGC_TOKEN', None),        help='Token of the account used to make the request (requires : \'api\' and \'sudo\' privileges)' )
-    parser.add_argument( '--gitlab_url',   '-g', metavar='RGC_GITLAB_URL',   default=os.environ.get('RGC_GITLAB_URL', None),   help='The GitLab URL that you want to access.' )
-    parser.add_argument( '--registry_url', '-d', metavar='RGC_REGISTRY_URL', default=os.environ.get('RGC_REGISTRY_URL', None), help='The GitLab Registry URL that you want to access.' )
-    parser.add_argument( '--retention',    '-r', metavar='RGC_RETENTION',    default=os.environ.get('RGC_RETENTION', '30'),    help='Remove tags older than this value (days) - defaults to 30' )
-    parser.add_argument( '--exclude',      '-e', metavar='RGC_EXCLUDE',      default=os.environ.get('RGC_EXCLUDE', '^latest'), help='Regex to exclude from the cleanup - defaults to \'^latest\'' )
+    parser = argparse.ArgumentParser(description='Cleanup old tags from GitLab Docker Registry')
+    parser.add_argument('--user', '-u', metavar='RGC_USER', default=os.environ.get('RGC_USER', None),
+                        help='Username of the account used to make the request')
+    parser.add_argument('--token', '-t', metavar='RGC_TOKEN', default=os.environ.get('RGC_TOKEN', None),
+                        help='Token of the account used to make the request (requires : \'api\' and \'sudo\' privileges)')
+    parser.add_argument('--gitlab_url', '-g', metavar='RGC_GITLAB_URL', default=os.environ.get('RGC_GITLAB_URL', None),
+                        help='The GitLab URL that you want to access.')
+    parser.add_argument('--registry_url', '-d', metavar='RGC_REGISTRY_URL',
+                        default=os.environ.get('RGC_REGISTRY_URL', None),
+                        help='The GitLab Registry URL that you want to access.')
+    parser.add_argument('--retention', '-r', metavar='RGC_RETENTION', default=os.environ.get('RGC_RETENTION', '30'),
+                        help='Remove tags older than this value (days) - defaults to 30')
+    parser.add_argument('--exclude', '-e', metavar='RGC_EXCLUDE', default=os.environ.get('RGC_EXCLUDE', '^latest'),
+                        help='Regex to exclude from the cleanup - defaults to \'^latest\'')
+    parser.add_argument('--project_filter', '-f', metavar='RGC_PROJECT_FILTER',
+                        default=os.environ.get('RGC_PROJECT_FILTER', '.*'),
+                        help='Regex to exclude projects from cleanup - defaults to \'.*\'')
     args = parser.parse_args()
 
     GitlabClean(
-        user         = args.user,
-        token        = args.token,
-        gitlab_url   = args.gitlab_url,
-        registry_url = args.registry_url,
-        retention    = args.retention,
-        exclude      = args.exclude,
+        user=args.user,
+        token=args.token,
+        gitlab_url=args.gitlab_url,
+        registry_url=args.registry_url,
+        retention=args.retention,
+        exclude=args.exclude,
+        project_filter=args.project_filter,
     ).clean_projects()
 
     sys.exit(0)
--- rgc/gitlab/clean.py
+++ rgc/gitlab/clean.py
@@ -6,30 +6,35 @@ from datetime import datetime
 from rgc.registry.api import RegistryApi
 from termcolor import colored
 
-class GitlabClean( object ):
-    def __init__( self, user, token, gitlab_url, registry_url, retention, exclude ):
-       self.user         = user
-       self.token        = token
-       self.gitlab_url   = gitlab_url
-       self.registry_url = registry_url
-       self.retention    = retention
-       self.exclude      = exclude
 
-    def clean_projects( self ):
+class GitlabClean(object):
+    def __init__(self, user, token, gitlab_url, registry_url, retention, exclude, project_filter):
+        self.user = user
+        self.token = token
+        self.gitlab_url = gitlab_url
+        self.registry_url = registry_url
+        self.retention = retention
+        self.exclude = exclude
+        self.project_filter = project_filter
+
+    def clean_projects(self):
         registry = RegistryApi(
-            user  = self.user,
-            token = self.token
+            user=self.user,
+            token=self.token
         )
 
         now = datetime.now()
 
         print(' -> fetch registry catalog...')
-        images = registry.query( self.registry_url + '/v2/_catalog?n=9999', 'get' )["repositories"]
+        images = registry.query(self.registry_url + '/v2/_catalog?n=9999', 'get')["repositories"]
 
-        print( '-> loading all projects..' )
-        for project in gitlab.Gitlab( self.gitlab_url, self.token ).projects.list( all=True ):
+        print('-> loading all projects..')
+        for project in gitlab.Gitlab(self.gitlab_url, self.token).projects.list(all=True):
+            if not re.match(self.project_filter, project.path_with_namespace.lower()):
+                print('-> filtered ' + project.path_with_namespace.lower())
+                continue
             if not project.container_registry_enabled:
-                print( '-> skipping ' + project.path_with_namespace.lower() )
+                print('-> skipping ' + project.path_with_namespace.lower())
                 continue
 
             subimages = []
@@ -39,31 +44,33 @@ class GitlabClean( object ):
                     subimages.append(image)
 
             for subimage in subimages:
-                print( '-> processing ' + subimage )
-                query_tags = registry.query( self.registry_url + '/v2/' + subimage + '/tags/list', 'get' )
+                print('-> processing ' + subimage)
+                query_tags = registry.query(self.registry_url + '/v2/' + subimage + '/tags/list', 'get')
                 tags = query_tags.get('tags', [])
 
                 if not tags:
-                    print( '--> no tags' )
+                    print('--> no tags')
                     continue
 
-                print( '--> ' + str( len( tags ) ) + ' tag(s) found' )
+                print('--> ' + str(len(tags)) + ' tag(s) found')
                 for tag in tags:
-                    if re.match( self.exclude, tag ):
-                        print( colored( '--> keeping ' + tag + ' (excluded)', 'green' ) )
+                    if re.match(self.exclude, tag):
+                        print(colored('--> keeping ' + tag + ' (excluded)', 'green'))
                         continue
 
                     # BUG: Sometimes the 'history' field is not available, usally works on next try
-                    tag_info = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'get' )
+                    tag_info = registry.query(self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'get')
                     if not 'history' in tag_info:
-                        print( colored( '--> couldn\'t get date info for ' + tag + ' (skipped)', 'yellow' ) )
+                        print(colored('--> couldn\'t get date info for ' + tag + ' (skipped)', 'yellow'))
                         continue
 
-                    created_at = datetime.strptime( json.loads( tag_info['history'][0]['v1Compatibility'] )['created'][:-4], '%Y-%m-%dT%H:%M:%S.%f' )
+                    created_at = datetime.strptime(
+                        json.loads(tag_info['history'][0]['v1Compatibility'])['created'][:-4], '%Y-%m-%dT%H:%M:%S.%f')
                     age = now - created_at
-                    if age.total_seconds() > ( int( self.retention ) * 60 * 60 * 24 ):
-                        print( colored( '--> removing ' + tag + ' (expired)', 'red' ) )
-                        digest = registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'head' )['Docker-Content-Digest']
-                        registry.query( self.registry_url + '/v2/' + subimage + '/manifests/' + digest, 'delete' )
+                    if age.total_seconds() > (int(self.retention) * 60 * 60 * 24):
+                        print(colored('--> removing ' + tag + ' (expired)', 'red'))
+                        digest = registry.query(self.registry_url + '/v2/' + subimage + '/manifests/' + tag, 'head')[
+                            'Docker-Content-Digest']
+                        registry.query(self.registry_url + '/v2/' + subimage + '/manifests/' + digest, 'delete')
                     else:
-                        print( colored( '--> keeping ' + tag + ' (not expired)', 'green' ) )
+                        print(colored('--> keeping ' + tag + ' (not expired)', 'green'))
